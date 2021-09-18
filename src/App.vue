5dd6a74846f1b5c6ecebe977bd5121034a15138b
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-09-18 03:08:02 +0200

    use short way to register the component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
     <the-header></the-header>
-    <!-- can be like that -->
-    <TheHeader />
-    <!-- or can be like that-->
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -18,7 +15,7 @@ import TheHeader from "./components/TheHeader.vue";
 
 export default {
   components: {
-    TheHeader: TheHeader, // register component locally
+    TheHeader, // register component locally
   },
   data() {
     return {
