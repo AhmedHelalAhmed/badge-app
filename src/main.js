99c113068dbc8b2edd65103cc47c966bc63f60eb
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-09-18 03:11:06 +0200

    Register BadgeList and UserInfo as local components

--- src/main.js
+++ src/main.js
@@ -2,15 +2,11 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import BaseBadge from "./components/BaseBadge.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
 
 const app = createApp(App);
 
 /* register global components */
 app.component("base-badge", BaseBadge);
-app.component("badge-list", BadgeList);
-app.component("user-info", UserInfo);
 /* register global components */
 
 app.mount("#app");
