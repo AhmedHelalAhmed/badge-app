c879ef7ef5979d27ae71c03bcb76146f68ec0672
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-09-18 03:59:58 +0200

    use shortcut way # instead of v-slot:

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,11 +1,13 @@
 <template>
   <section>
     <base-card>
-      <template v-slot:header>
+      <template #header>
         <h3>{{ fullName }}</h3>
         <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
       </template>
-      <p>{{ infoText }}</p>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
     </base-card>
   </section>
 </template>
@@ -17,4 +19,3 @@ export default {
   props: ["fullName", "infoText", "role"],
 };
 </script>
-
