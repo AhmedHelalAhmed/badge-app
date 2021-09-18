6710742121423007fe85c2b6b85a385426f66e7a
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-09-18 11:46:17 +0200

    get data from slot and use it in the template content given

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,13 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <!-- slotProps any name -->
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,12 +21,14 @@
 import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo, // register component locally
+    UserInfo,
+    CourseGoals, // register component locally
   },
   data() {
     return {
